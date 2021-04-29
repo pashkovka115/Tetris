75b638c161fefed93e64ee28e26745cf31879789
Sergey <pariss8@mail.ru>
2021-04-29 07:01:31 +0300

    Добавил проект Enum

---
+++ Enums/Day.cs
@@ -0,0 +1,7 @@
+﻿namespace Enums
+{
+    public enum Day : byte
+    {
+        Mon = 1, Tue, Wed, Thu, Fri, Sat, Sun
+    }
+}
---
+++ Enums/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day day = Day.Sat;
+
+            if (day == Day.Sat)
+            {
+                Console.WriteLine("YES!");
+            }
+
+            int day3 = 3;
+
+            Console.WriteLine( (Day)day3 );
+        }
+    }
+}
